0aef27d0464a69f4bdad71794d9134adcf981ab5
Guillaume <guizanko@hotmail.fr>
2024-01-25 11:53:00 +0100

    why

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import "./App.css"
 
 function App() {
   
 
   return (
     <>
-     
+     <div class="bg-black border border-black">hello</div>
+     <div className='bg black border border-black'>jijizjrgezrg</div>
     </>
   )
 }
