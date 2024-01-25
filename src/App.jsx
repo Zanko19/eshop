3510c2bcc2c3f343815430f3b35139bba794df8e
Guillaume <guizanko@hotmail.fr>
2024-01-25 12:21:12 +0100

    ooooktwcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import "./App.css"
 
 function App() {
   
 
   return (
     <>
-     <div class="bg-black border border-black">hello</div>
-     <div className='bg black border border-black'>jijizjrgezrg</div>
+     <div className="text-red">hello</div>
+     <div className='border border-black text-blue-500'>jijizjrgezrg</div>
     </>
   )
 }
